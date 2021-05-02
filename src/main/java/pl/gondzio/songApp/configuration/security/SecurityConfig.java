03e5ede708445628fca18a851997069fa7a9dbc5
gondzio00 <gondzio00@gmail.com>
2021-05-02 16:33:36 +0200

    2

--- src/main/java/pl/gondzio/songApp/configuration/security/SecurityConfig.java
+++ src/main/java/pl/gondzio/songApp/configuration/security/SecurityConfig.java
@@ -77,8 +77,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // Enable CORS and disable CSRF
-        http = http.cors().disable();
-        http = http.csrf().disable();
+        http = http.cors().and().csrf().disable();
 
         // Set session management to stateless
         http = http
